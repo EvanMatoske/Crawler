b2799077fd3370f0d2f3a208c79cb01a3627e295
Evan Matoske <evanmatoske@gmail.com>
2018-04-26 23:06:09 -0400

    first commit

---
+++ crawler.js
@@ -0,0 +1,95 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+
+const fs = require('fs');
+const path = require('path');
+let jsonfile = require('jsonfile');
+
+let uniqid = require('uniqid');
+
+var START_URL = "http://www.arstechnica.com";
+var SEARCH_WORD = "stemming";
+var MAX_PAGES_TO_VISIT = 10;
+
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+
+let ID = uniqid();
+console.log(ID);
+
+let search = {
+  "ID": ID,
+  "Sites":[]
+};
+
+
+let filename = path.join(__dirname, ID + '.json')
+
+jsonfile.writeFileSync(filename, search,{spaces:2});
+pagesToVisit.push(START_URL);
+crawl();
+
+function crawl() {
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  var nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  } else {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
+
+function visitPage(url, callback) {
+  // Add page to our set
+  pagesVisited[url] = true;
+  numPagesVisited++;
+
+  // Make the request
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Parse the document body
+     var $ = cheerio.load(body);
+     var isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       
+       var title = $("title").text();
+       search = jsonfile.readFileSync(filename);
+       search.Sites.push({'URL':url, 'Title':title});
+       jsonfile.writeFileSync(filename, search, { spaces: 2 });
+
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
+
+function searchForWord($, word) {
+  var bodyText = $('html > body').text().toLowerCase();
+  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+}
+
+function collectInternalLinks($) {
+    var relativeLinks = $("a[href^='/']");
+    console.log("Found " + relativeLinks.length + " relative links on page");
+    relativeLinks.each(function() {
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
+    });
+}
